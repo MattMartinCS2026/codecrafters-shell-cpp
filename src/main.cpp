6ababe5050c39c346dbc59b2d089ac32ce2cd79d
matt <mattmartin2004@gmail.com>
2026-08-25 16:07:38 -0400

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
 #include <string>
 
+void eval(std::string command) {
+  std::cout << command << ": command not found\n";
+}
+
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
   std::string command;
-
-  std::cout << "$ ";
-  std::cin >> command;
-  std::cout << command << ": command not found\n";
+  while (true) {
+    std::cout << "$ ";
+    std::cin >> command;
+    eval(command);
+  }
 
   return 0;
 }
